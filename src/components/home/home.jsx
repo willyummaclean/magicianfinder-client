c6eb6e3e465c04b38665e5d13904a37d81b58d5e
Will MacLean <maclean.will@gmail.com>
2024-04-26 10:20:05 -0500

    template

---
+++ src/components/home/home.jsx
@@ -0,0 +1,46 @@
+
+import "./Home.css"
+
+
+export const Home = () => {
+    return (
+        <>
+        <div>
+        <div className="container">
+        <div className="title-container">
+            <h1 className="title">
+                <span>Magician Finder</span>
+            </h1>
+        </div>
+          
+        <div className="block">
+            <div className="block--img block1"></div>
+            <div className="block--text">
+            <div className="text--head">Illusions</div>
+            <div className="text--quote">“Magic is a lie, but magician need not be a liar.”</div>
+            <div className="text--person">Amit Kalantri</div>
+            </div>
+        </div>
+
+        <div className="block">
+            <div className="block--img block2"></div>
+            <div className="block--text">
+            <div className="text--head">Presto-Change-o</div>
+            <div className="text--quote">“Sleight of hand is the magic of the mind.”</div>
+            <div className="text--person">Ricky Jay</div>
+            </div>
+        </div>
+
+        <div className="block">
+            <div className="block--img block3"></div>
+            <div className="block--text">
+            <div className="text--head">Abra-cadabra</div>
+            <div className="text--quote">“Drive it like you stole it!”</div>
+            <div className="text--person">The Amazing Johnathan</div>
+            </div>
+        </div>
+        </div>
+        </div>
+        </>
+    )
+}
