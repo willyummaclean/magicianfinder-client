a0fe1beb95ad8026fde3f850135aa3308224662f
Will MacLean <maclean.will@gmail.com>
2024-04-26 20:29:14 -0500

    home page on home page

--- src/components/ApplicationViews.jsx
+++ src/components/ApplicationViews.jsx
@@ -1,35 +1,3 @@
-// import { useEffect, useState } from "react"
-// import { Route, Routes, Outlet } from "react-router-dom"
-// import { Home } from "../components/home/home.jsx"
-// import { Navbar } from "../components/nav/Navbar.jsx"
-
-
-// export const ApplicationViews = () => {
-
-//     const [currentUser, setCurrentUser] = useState({})
-
-// useEffect(() => {
-//     const localMagicUser = localStorage.getItem("magic_token")
-//     const MagicUser = JSON.parse(localMagicUser)
-//         setCurrentUser(MagicUser)
-//     }, [])
-    
-
-//     return <Routes>
-//             <Route
-//                  path="/"
-//                  element={
-//             <>
-//                 <Navbar />
-//                 <Outlet />
-//             </>
-//             }
-//             >
-//             <Route index element={<Home currentUser={currentUser}/>}/> 
-          
-//             </Route>
-//         </Routes>
-// }
 
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Authorized } from "./Authorized"
