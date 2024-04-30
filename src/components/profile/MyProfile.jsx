1161743ea61f7435e531416a2c47f6775724fec1
Will MacLean <maclean.will@gmail.com>
2024-04-29 19:50:05 -0500

    current user

--- src/components/profile/MyProfile.jsx
+++ src/components/profile/MyProfile.jsx
@@ -1,8 +1,16 @@
-
-
+import { useEffect, useState } from "react"
+import { getUser } from "../../data/ProfileData"
 
 
 export const MyProfile = () => {
-    
+    const [user, setUser] = useState({})
+    const [participant, setParticipant] = useState({})
+
+    useEffect(() => {
+        getUser().then((data) => setUser(data))
+        if (user); {
+            getParticipant(user.id).then((p) => setParticipant(p))
+        }
+    },[])
 
 }
